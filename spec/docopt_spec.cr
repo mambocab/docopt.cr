ad6811650c4a69e32ffc8950be83a32db1145fdc
Jim Witschey <jim.witschey@gmail.com>
2016-03-15 23:57:48 -0400

    adds utility methods for parsing options: lines

--- spec/docopt_spec.cr
+++ spec/docopt_spec.cr
@@ -14,5 +14,64 @@ describe Docopt do
                   "\n"
       Docopt.docopt(docstring, argv: [] of String).should eq({"-a" => false})
     end
+
+    pending "parses arbitrary option names" do
+      docstring = "Usage: prog [options]\n"\
+                  "\n"\
+                  "Options: -b  All.\n"\
+                  "\n"
+      Docopt.docopt(docstring, argv: [] of String).should eq({"-b" => false})
+    end
+  end
+end
+
+describe DocoptUtil::StringUtil do
+  describe "#get_option_lines" do
+    pending "returns the empty string if there is no 'options:' section" do
+      DocoptUtil::StringUtil.get_option_lines("Usage: prog\n\n").should eq([] of String)
+    end
+
+    pending "finds the 'options:' section on simple 2-part docstring" do
+      docstring = "Usage: prog [options]\n"\
+                  "\n"\
+                  "Options: -b  All.\n"\
+                  "\n"
+    end
+  end
+end
+
+describe DocoptUtil::ArrayUtil do
+  describe "#take_chunks_starting_with_selected" do
+    it "returns the empty array on the empty array" do
+      (DocoptUtil::ArrayUtil.take_chunks_starting_with_selected([] of String) { |x| true })
+        .should eq([] of Array(String))
+    end
+
+    it "skips all elements if func(e) is always false" do
+      (DocoptUtil::ArrayUtil.take_chunks_starting_with_selected([1, 1, 1, 1, 1]) { |x| false })
+        .should eq([] of Array(Int32))
+    end
+
+    it "skips initial elements if func(e) starts false" do
+      (DocoptUtil::ArrayUtil.take_chunks_starting_with_selected([10, 11]) { |x| x == 11 })
+        .should eq([[11]])
+    end
+
+  end
+
+  describe "#indices_where" do
+    it "returns the empty array given the empty array" do
+      DocoptUtil::ArrayUtil.indices_where([] of String) { |x| true }
+        .should eq([] of Int32)
+    end
+
+    it "returns all indices if the condition is always true" do
+      DocoptUtil::ArrayUtil.indices_where([1, 2, "foo"]) { |x| true }.should eq([0, 1, 2])
+    end
+
+    it "returns indices where the condition is true" do
+      DocoptUtil::ArrayUtil.indices_where([1, 2, 3, 4, 5]) { |x| x % 2 == 0 }
+        .should eq([1, 3])
+    end
   end
 end
--- src/docopt.cr
+++ src/docopt.cr
@@ -7,3 +7,31 @@ module Docopt
     end
   end
 end
+
+module DocoptUtil
+  # Utilities for manipulating strings
+  module StringUtil
+    def self.get_option_lines(s)
+      split = s.split('\n').reject { |x| x == "" }
+    end
+  end
+
+  module ArrayUtil
+    # Given an Iterable or Enumerable xs, return its elements in the same order
+    # but chunked into arrays that start with an element e for which func(e)
+    # is true, but for which func(f) is false for each subsequent value f. For
+    # example:
+    #
+    #     ary = [0, 1, 2, 3, 1, 4, 5, 1]
+    #     take_chunks_starting_with_selected(ary, { |x| x == 1 }) #=> [[1, 2, 3], [1, 4, 5], [1]]
+    def self.take_chunks_starting_with_selected(xs :(Iterable(T) | Enumerable(T)), &block : T -> _)
+      xs = xs.to_a
+      self.indices_where(xs, &block).map { |i| [xs[i]] }
+    end
+
+    def self.indices_where(xs : (Iterable(T) | Enumerable(T)), &block : T -> _)
+      xs.each.with_index.select { |x| block.call(x.first) }.to_a.map { |x| x.last }
+    end
+  end
+end
+
